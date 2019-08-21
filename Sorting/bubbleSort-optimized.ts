e39f9c5db426dc329354b1bf0ba325cea448a4ba
Eduar Apaza <educartoons@gmail.com>
2019-08-20 21:59:32 -0500

    Added BubbleSort Optimized

--- Sorting/bubbleSort-optimized.ts
+++ Sorting/bubbleSort-optimized.ts
@@ -1,18 +1,21 @@
-// BubbleSort
+// BubbleSort Optimized
 // A sorting algorithm where the largest values bubble up to the top
 
 function bubbleSort(arr: number[]): number[] {
   for (let i: number = arr.length; i > 0; i--) {
+    let noSwaps: boolean;
     for (let j: number = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         //swap
         let temp: number = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        noSwaps = false;
       }
     }
+    if (noSwaps) break;
   }
   return arr;
 }
 
-console.log(bubbleSort([37, 45, 29, 8]));
+console.log(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]));
